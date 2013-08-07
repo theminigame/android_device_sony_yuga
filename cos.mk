3e70e1595bd18ccac5027bcbe5553edefac2a6d5
theminigame <tehminigame@gmail.com>
2013-08-07 00:28:27 -0600

    Update cos.mk

--- cos.mk
+++ cos.mk
@@ -11,5 +11,5 @@ $(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
 
-PRODUCT_NAME := chaos_yuga
+PRODUCT_NAME := cos_yuga
 PRODUCT_DEVICE := yuga
